33158aa57effd8747ba708d359bfd8637f09ee24
Jeong, Huiwon <hjeong303@gatech.edu>
2020-08-16 00:38:25 -0700

    Add views & setup global variables for views

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,6 @@
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
+export const login = (req, res) => res.render("Login");
+export const join = (req, res) => res.render("Join");
+export const users = (req, res) => res.render("Users");
+export const editProfile = (req, res) => res.render("Edit Profile");
+export const changePassword = (req, res) => res.render("Change Password");
+export const userDetail = (req, res) => res.render("User Detail");
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,19 @@
 import express from "express";
 import routes from "../routes";
+import {
+  videoDetail,
+  videos,
+  editVideo,
+  upload,
+  deleteVideo,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, (req, res) => res.send("Video"));
-videoRouter.get(routes.upload, (req, res) => res.send("Upload"));
-videoRouter.get(routes.editVideo, (req, res) => res.send("EditVideo"));
-videoRouter.get(routes.videoDetail, (req, res) => res.send("VideoDetail"));
-videoRouter.get(routes.deleteVideo, (req, res) => res.send("DeleteVideo"));
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
