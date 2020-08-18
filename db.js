15b2281ed7ff66c370cc7e4aec2bc4460b27ab83
Jeong, Huiwon <hjeong303@gatech.edu>
2020-08-17 21:33:51 -0700

    Define video comment models

--- db.js
+++ db.js
@@ -1,56 +1,17 @@
-const { lstat } = require("fs");
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-export const videos = [
-  {
-    id: 322214,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 123123,
-      name: "Huiwon",
-      email: "huiwon!@lstat.com",
-    },
-  },
-  {
-    id: 33556,
-    title: "Video super",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 123123,
-      name: "Huiwon",
-      email: "huiwon!@lstat.com",
-    },
-  },
-  {
-    id: 4445433,
-    title: "Video nice",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 123123,
-      name: "Huiwon",
-      email: "huiwon!@lstat.com",
-    },
-  },
-  {
-    id: 889977,
-    title: "Video perfect",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 123123,
-      name: "Huiwon",
-      email: "huiwon!@lstat.com",
-    },
-  },
-];
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = error => console.log(`❌ Error on DB Connection: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
